d6eb4e777af1a8cd833f53075355986606532e29
Jay, Jeongheum <jh0413.lee@gmail.com>
2021-06-21 07:07:33 +0900

    Update ex_telegram_bot.py

--- ex_telegram_bot.py
+++ ex_telegram_bot.py
@@ -1,11 +1,19 @@
 import telegram
-
-Jay_noti_bot_Token="1805573611:AAHw6pHuuJRlwQH_UbEIHtnM4pLtyDJcvSY"
-botID=1805573611
-myID=1259062846
+import json
+with open('personal.json','r') as f:
+  json_data = json.load(f)
+  
+Jay_noti_bot_Token=json_data["telegram"]["token"]
+myID=json_data["telegram"]["id"]
 bot = telegram.Bot(token = Jay_noti_bot_Token)
 # for i in bot.getUpdates():
 #     print(i.message)
 #print(bot.get_me())
-bot.sendMessage(chat_id = myID, text='안녕하세요 만나서 반가워요')
-bot.send_photo(chat_id= myID, open('ss.png','rb'))
+
+# send text message
+txt='안녕하세요 만나서 반가워요'
+bot.sendMessage(chat_id = myID, text=txt)
+
+# send photo
+img_url= url
+bot.send_photo(chat_id= myID, photo=img_url)
